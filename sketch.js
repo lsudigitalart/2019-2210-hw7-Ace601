00d3430cfb04868688823b806f46b81e9339936b
Angel <Sproski88@gmail.com>
2019-10-22 13:16:59 -0500

    work

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let song;
+
+
+function preload(){
+    song = loadSound('Clint Mansell - Lux Aeterna (Requiem for a Dream).mp3')
+
+}
+
+
+function setup(){
+createCanvas(800,800)
+
+}
+
+
+
+function draw(){
+
+
+} 
