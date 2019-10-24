cf30c674b1d76185e69db58bfd73d119ace2f1ef
Angel <Sproski88@gmail.com>
2019-10-24 04:59:08 -0500

    final

--- sketch.js
+++ sketch.js
@@ -1,20 +1,53 @@
 let song;
+let img;
+let img1;
+let str = '20';
+let diameter = float(str)
+var fall,[];
+class Drop{
+    constructor(){
+float; x = random (width)
+float; y = random (-200,-100)
+float; yspeed = random (4,10)
+float; z = random(10,20)
+float; length = random(10,20)
+    }
+
+        }
+
+
+
+function show(){
+strokeWeight
+
+}
 
 
 function preload(){
     song = loadSound('Clint Mansell - Lux Aeterna (Requiem for a Dream).mp3')
-
+    img = loadImage('New Composition_1.png');
+    img1 = loadImage('New Composition_2.png')
 }
 
 
 function setup(){
 createCanvas(800,800)
 
+image(img, 0, 0);
+image(img1, 0, 0);
+
 }
 
 
 
-function draw(){
+function draw(){}
 
 
-} 
+class fall {
+    constructor() {
+        this.y = y + yspeed;
+        this.yspeed = yspeed + 0.1;
+        if (y > height) {
+            this.y = random(-200, -100);
+            this.yspeed = random(4, 10);}
+        }}
