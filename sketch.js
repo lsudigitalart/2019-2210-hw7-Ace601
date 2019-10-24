b3b38f0a5e688edffd446e28f182d3e4b94949ad
Angel <Sproski88@gmail.com>
2019-10-24 14:48:19 -0500

    final

--- sketch.js
+++ sketch.js
@@ -2,25 +2,29 @@ let song;
 let img;
 let img1;
 let str = '20';
-let diameter = float(str)
-var fall,[];
-class Drop{
-    constructor(){
-float; x = random (width)
-float; y = random (-200,-100)
-float; yspeed = random (4,10)
-float; z = random(10,20)
-float; length = random(10,20)
-    }
+// let diameter = float(str)
+var fall = [];
 
-        }
+// class Drop{
+//     constructor(){
+//         this.x = random (width)
+//         this.y = random (-200,-100)
+//         this.yspeed = random (4,10)
+//         this.z = random(10,20)
+//         this.length = random(10,20)
+//     }
+
+//     display(){
+
+//     }
+// }
 
 
 
-function show(){
-strokeWeight
+// function show(){
+// strokeWeight
 
-}
+// }
 
 
 function preload(){
@@ -29,25 +33,35 @@ function preload(){
     img1 = loadImage('New Composition_2.png')
 }
 
+var y1 = 0;
+var y2 = 0;
+
 
 function setup(){
 createCanvas(800,800)
 
-image(img, 0, 0);
-image(img1, 0, 0);
+
 
 }
 
+function mousePressed() {
+    song.play();
+}
+
+function draw(){
+    background(255);
+    image(img, width/2, y1);
+    image(img1, width/20, y2);
+    y1++;
+    y2+=2;
+}
 
 
-function draw(){}
-
-
-class fall {
-    constructor() {
-        this.y = y + yspeed;
-        this.yspeed = yspeed + 0.1;
-        if (y > height) {
-            this.y = random(-200, -100);
-            this.yspeed = random(4, 10);}
-        }}
+// class fall {
+//     constructor() {
+//         this.y = y + yspeed;
+//         this.yspeed = yspeed + 0.1;
+//         if (y > height) {
+//             this.y = random(-200, -100);
+//             this.yspeed = random(4, 10);}
+//         }}
